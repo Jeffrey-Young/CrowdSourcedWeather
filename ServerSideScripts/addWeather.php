481e53414fc613bbbd0d9640dca802bc738f1323
Ubuntu <ubuntu@ip-172-31-26-226.us-west-2.compute.internal>
2017-12-02 07:36:01 +0000

    fully functional insertion of new weather items

--- ServerSideScripts/addWeather.php
+++ ServerSideScripts/addWeather.php
@@ -1,10 +1,32 @@
 <?php
+/*	Author: Connor Hamlet.
+*	Description:
+*   This php script is used for adding weather items into the database. 
+*   Input type: JSON. Structure:   long: decimal, lat: decimal, 
+*	weather: description < 15 chars
+*
+*      Here is the curl command for testing purposes. 
+*
+*	 curl -H "Content-Type: application/json" -X 
+*	POST{"long":123.234,"lat":34.5432, "weather":"rain"}' 
+*	https://weatherclone.hopto.org/addWeather.php
+*	
+*	Return type: JSON. 
+*	Returns:   a json with:  "received": 0 if there is an error, 
+*   and "received": 1 is the insertion completed successfully. 
+*/
+//Decodes JSON
+$_POST = json_decode(file_get_contents('php://input'), true);
+//Expecting:
+//long: Decimal
+//lat: Decimal
+//weather: string (rain, sun, storm)
 
 $returndata = array(
-    'isAuthenticated'      => 0
+    'received'      => 0
 );
-
-if(empty($_GET["username"])){
+//If any of the fields are empty, return. 
+if(empty($_POST["long"]) ||empty($_POST["lat"]) || empty($_POST["weather"])){
 	$json = json_encode($returndata);
 	echo $json;
 	return;
@@ -15,27 +37,23 @@ if (!$con) {
     echo "Error: " . mysqli_connect_error();
 	exit();
 }
-$stmt = mysqli_prepare($con, "SELECT * FROM users WHERE username=?");
-mysqli_stmt_bind_param($stmt, 's', $user);
-$user = $_GET["username"];
+//Preparing statement, prevents sql injection. This will add
+//the weather item to table. 
+$stmt = mysqli_prepare($con, "INSERT INTO `weatherItems`(".
+		" `latitude`, `longitude`, `weather`, `time`)".
+		" VALUES (?,?,?,UNIX_TIMESTAMP())");
+mysqli_stmt_bind_param($stmt, 'dds', $lat, $long, $weather);
+$lat = $_POST["lat"];
+$long = $_POST["long"];
+$weather = $_POST["weather"];
 
-mysqli_stmt_execute($stmt);
-/* bind result variables */
-mysqli_stmt_bind_result($stmt, $id, $name, $pass);
 
-mysqli_stmt_store_result($stmt);
-if (mysqli_stmt_num_rows($stmt) < 1){
+if(mysqli_stmt_execute($stmt)){
+        $returndata["received"] = 1;
         $json = json_encode($returndata);
         echo $json;
         return;
 }
-/* fetch values */
-    while (mysqli_stmt_fetch($stmt)) {
-	$returndata["isAuthenticated"] = 1;
-        $json = json_encode($returndata);
-        echo $json;
-        return;
-    }
-
+/* bind result variables */
 mysqli_stmt_close($stmt);
 mysqli_close($con);
