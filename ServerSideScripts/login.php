fe54b6756b679fb1838a8fcf3361bcde45d930d5
Ubuntu <ubuntu@ip-172-31-26-226.us-west-2.compute.internal>
2017-12-02 22:16:44 +0000

    handles naive user authentication

--- ServerSideScripts/login.php
+++ ServerSideScripts/login.php
@@ -1,10 +1,14 @@
 <?php
 
+header('Content-Type: application/json');
+//Decodes JSON
+$_POST = json_decode(file_get_contents('php://input'), true);
+
 $returndata = array(
     'isAuthenticated'      => 0
 );
 
-if(empty($_GET["username"])){
+if(empty($_POST["username"]) || empty($_POST["password"])){
 	$json = json_encode($returndata);
 	echo $json;
 	return;
@@ -15,14 +19,14 @@ if (!$con) {
     echo "Error: " . mysqli_connect_error();
 	exit();
 }
-$stmt = mysqli_prepare($con, "SELECT * FROM users WHERE username=?");
-mysqli_stmt_bind_param($stmt, 's', $user);
-$user = $_GET["username"];
+$stmt = mysqli_prepare($con, "SELECT * FROM users WHERE username=? AND password=?");
+mysqli_stmt_bind_param($stmt, 'ss', $user, $pass);
+$user = $_POST["username"];
+$pass = $_POST["password"];
 
 mysqli_stmt_execute($stmt);
 /* bind result variables */
-mysqli_stmt_bind_result($stmt, $id, $name, $pass);
-
+mysqli_stmt_bind_result($stmt, $id, $name, $password);
 mysqli_stmt_store_result($stmt);
 if (mysqli_stmt_num_rows($stmt) < 1){
         $json = json_encode($returndata);
@@ -34,7 +38,6 @@ if (mysqli_stmt_num_rows($stmt) < 1){
 	$returndata["isAuthenticated"] = 1;
         $json = json_encode($returndata);
         echo $json;
-        return;
     }
 
 mysqli_stmt_close($stmt);
